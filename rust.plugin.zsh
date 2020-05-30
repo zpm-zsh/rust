1dfd40eaf359004069d149f525c9f2a2015f4db3
Grigorii Horos <horosgrisa@gmail.com>
2020-05-30 22:56:29 +0300

    Fix

--- rust.plugin.zsh
+++ rust.plugin.zsh
@@ -13,10 +13,10 @@ appendpath "${HOME}/.cargo/bin"
 local CACHE_DIR="${TMPDIR:-/tmp}/zsh-${UID}"
 
 if [[ ! -f "${CACHE_DIR}/functions/_rustup" ]]; then
-  rustup completions zsh > "${CACHE_DIR}/functions/_rustup"
+  rustup completions zsh > "${CACHE_DIR}/functions/_rustup" 2>/dev/null
 fi
 
 if [[ ! -f "${CACHE_DIR}/functions/_cargo" ]]; then
-  cp "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "${CACHE_DIR}/functions/_cargo"
+  cp "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "${CACHE_DIR}/functions/_cargo" 2>/dev/null
 fi
 
