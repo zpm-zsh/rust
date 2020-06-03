2c8e9a3d336f1b9409cfb4f746cdbd478749833f
Grigorii Horos <horosgrisa@gmail.com>
2020-06-03 17:11:04 +0300

    Refactoring

--- rust.plugin.zsh
+++ rust.plugin.zsh
@@ -1,22 +1,24 @@
 #!/usr/bin/env zsh
-# Standarized ZSH polyfills, following:
-# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-0="${${(M)0:#/*}:-$PWD/$0}"
 
-if [[ $PMSPEC != *f* ]] {
-  fpath+=( "${0:h}/functions" )
-}
+if (( $+commands[rustc] )); then
+  # Standarized ZSH polyfills, following:
+  # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+  0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+  0="${${(M)0:#/*}:-$PWD/$0}"
 
-appendpath "${HOME}/.cargo/bin"
+  if [[ $PMSPEC != *f* ]] {
+    fpath+=( "${0:h}/functions" )
+  }
 
-local CACHE_DIR="${TMPDIR:-/tmp}/zsh-${UID}"
+  appendpath "${HOME}/.cargo/bin"
 
-if [[ ! -f "${CACHE_DIR}/functions/_rustup" ]]; then
-  rustup completions zsh > "${CACHE_DIR}/functions/_rustup" 2>/dev/null
+  local CACHE_DIR="${TMPDIR:-/tmp}/zsh-${UID}"
+
+  if [[ ! -f "${CACHE_DIR}/functions/_rustup" ]]; then
+    rustup completions zsh > "${CACHE_DIR}/functions/_rustup" 2>/dev/null
+  fi
+
+  if [[ ! -f "${CACHE_DIR}/functions/_cargo" ]]; then
+    cp "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "${CACHE_DIR}/functions/_cargo" 2>/dev/null
+  fi
 fi
-
-if [[ ! -f "${CACHE_DIR}/functions/_cargo" ]]; then
-  cp "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "${CACHE_DIR}/functions/_cargo" 2>/dev/null
-fi
-
