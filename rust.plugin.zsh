d716f934aed0efe6b5afe80f0dde259793e0b446
Grigorii Horos <horosgrisa@gmail.com>
2020-07-29 16:31:59 +0300

    Fix generation

--- rust.plugin.zsh
+++ rust.plugin.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+  zpm load zpm-zsh/helpers #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+
 if (( $+commands[rustc] )); then
   # Standarized ZSH polyfills, following:
   # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
@@ -12,13 +16,10 @@ if (( $+commands[rustc] )); then
 
   appendpath "${HOME}/.cargo/bin"
 
-  local CACHE_DIR="${TMPDIR:-/tmp}/zsh-${UID}"
-
-  if [[ ! -f "${CACHE_DIR}/functions/_rustup" ]]; then
-    rustup completions zsh > "${CACHE_DIR}/functions/_rustup" 2>/dev/null
+  if [[ ! -f "${0:h}/functions/_rustup" ]]; then
+    rustup completions zsh > "${0:h}/functions/_rustup" #2>/dev/null
   fi
-
-  if [[ ! -f "${CACHE_DIR}/functions/_cargo" ]]; then
-    cp "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "${CACHE_DIR}/functions/_cargo" 2>/dev/null
+  if [[ ! -f "${0:h}/functions/_cargo" ]]; then
+    cp "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "${0:h}/functions/_cargo" # 2>/dev/null
   fi
 fi
